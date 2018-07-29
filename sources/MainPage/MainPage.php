a7cdd34a5831e6b1e4afd6401614c1b987d5527b
Ariel Bogdziewicz <ariel.bogdziewicz@gmail.com>
2018-07-30 01:15:53 +0200

    Added comments to MainPage class.

--- sources/MainPage/MainPage.php
+++ sources/MainPage/MainPage.php
@@ -1,27 +1,45 @@
 <?php
+/**
+ * @project BasePHP Framework
+ * @file MainPage.php created by Ariel Bogdziewicz on 29/07/2018
+ * @author Ariel Bogdziewicz
+ * @copyright Copyright © 2018 Ariel Bogdziewicz. All rights reserved.
+ * @license MIT
+ */
 namespace Project\MainPage;
 
 use Base\Core\Controller;
 use Base\Responses\Raw;
 use Base\Responses\Redirect;
 
+/**
+ * Class MainPage is example controller.
+ * @package Project\MainPage
+ */
 class MainPage extends Controller
 {
-    public function getOutput(): string
-    {
-        // TODO: Implement getOutput() method.
-    }
-
+    /**
+     * Test of empty route.
+     * @return Raw
+     */
     public function main()
     {
         return new Raw("MAIN");
     }
 
+    /**
+     * Test of simple route.
+     * @return Raw
+     */
     public function second()
     {
         return new Raw("SECOND");
     }
 
+    /**
+     * Test of redirect.
+     * @return Redirect
+     */
     public function redirect()
     {
         return new Redirect("/second");
